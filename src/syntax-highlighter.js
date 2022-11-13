83fd36e07ef22b349a63ffb0031731df4b6f9039
Daniel Post <daniel@danielpost.com>
2022-11-13 14:05:26 +0100

    Add line numbers, diffing and focus

--- src/syntax-highlighter.js
+++ src/syntax-highlighter.js
@@ -1,5 +1,6 @@
 import { toHtml } from 'hast-util-to-html';
 import { createStarryNight, common } from '@wooorm/starry-night';
+import { starryNightGutter } from './hast-util-starry-night-gutter.js';
 
 export async function highlight(code, lang) {
 	if (!code || !lang) {
@@ -10,7 +11,20 @@ export async function highlight(code, lang) {
 		code = `<?php\n${code}`;
 	}
 
+	code = `function blur() {
+	const isBlurred = true;//[hl ++]
+	console.log(isBlurred);//[hl --]
+	console.log({ isBlurred });//[hl focus]
+}
+
+blur();`;
+
+	lang = 'js';
+
 	const starryNight = await createStarryNight(common);
 	const tree = starryNight.highlight(code, starryNight.flagToScope(lang));
+
+	starryNightGutter(tree);
+
 	return toHtml(tree);
 }
