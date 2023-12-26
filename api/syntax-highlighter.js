1b6033bbaf336ce014edb2139a453f6ddc971121
Daniel Post <daniel@danielpost.com>
2023-12-26 18:17:55 -0500

    Switch to using body

--- api/syntax-highlighter.js
+++ api/syntax-highlighter.js
@@ -1,7 +1,13 @@
 import { highlight } from '../src/syntax-highlighter.js';
 
 export default async (request, response) => {
+	if (!request.body || !request.body.code || !request.body.lang) {
+		return response.status(400).json({
+			error: 'Missing code or lang',
+		});
+	}
+
 	response.status(200).json({
-		html: await highlight(request.query.code, request.query.lang),
+		html: await highlight(request.body.code, request.body.lang),
 	});
 };
