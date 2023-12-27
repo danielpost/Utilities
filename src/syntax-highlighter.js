befc057275ed981c2efea2a4a87e920db5565564
Daniel Post <daniel@danielpost.com>
2023-12-27 13:03:10 -0500

    Try local files

--- src/syntax-highlighter.js
+++ src/syntax-highlighter.js
@@ -1,4 +1,27 @@
 import { getHighlighter, loadTheme } from 'shiki';
+import * as fs from 'fs/promises';
+import { join as pathJoin } from 'path';
+//
+// Shiki loads languages and themes using "fs" instead of "import", so Next.js
+// doesn't bundle them into production build. To work around, we manually copy
+// them over to our source code (lib/shiki/*) and update the "paths".
+//
+// Note that they are only referenced on server side
+// See: https://github.com/shikijs/shiki/issues/138
+const getShikiPath = () => {
+	return pathJoin(process.cwd(), 'assets/shiki');
+};
+
+const touched = { current: false };
+
+// "Touch" the shiki assets so that Vercel will include them in the production
+// bundle. This is required because shiki itself dynamically access these files,
+// so Vercel doesn't know about them by default
+const touchShikiPath = () => {
+	if (touched.current) return; // only need to do once
+	fs.readdir(getShikiPath()); // fire and forget
+	touched.current = true;
+};
 
 export async function highlight(code, lang) {
 	if (!code || !lang) {
@@ -11,9 +34,14 @@ export async function highlight(code, lang) {
 	code = code.replace(/&#91;/g, '[');
 	code = code.replace(/&#93;/g, ']');
 
-	const theme = loadTheme('../../assets/one-dark-pro.json');
+	touchShikiPath();
+
 	const highlighter = await getHighlighter({
-		theme,
+		theme: 'one-dark-pro',
+		paths: {
+			languages: `${getShikiPath()}/languages/`,
+			themes: `${getShikiPath()}/themes/`,
+		},
 	});
 
 	const focusLines = [];
